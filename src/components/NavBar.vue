ddc2b6587996dd636b0ac97747837bd39b65b929
hchavezj <hchavezj@gmail.com>
2022-09-14 02:16:39 -0500

    Fixed and error in NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,18 +21,23 @@
 <style scoped lang="postcss">
   nav {
     @apply flex h-20 bg-slate-900 text-slate-200;
+
     .wrapper {
       @apply container mx-auto flex w-full items-center justify-between;
+
       .brand {
         &-title {
           @apply text-2xl font-bold text-yellow-500;
         }
       }
+
       .menu {
         @apply flex gap-2;
+
         &-item {
           @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
         }
+
         &-login {
           @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
         }
