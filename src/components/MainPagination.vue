42cd61a8ca2bf7c86b090289aa3ad626729220e6
hchavezj <hchavezj@gmail.com>
2022-09-14 22:39:35 -0500

    Added Main.Pagination.vue

---
+++ src/components/MainPagination.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref } from 'vue'
+  const pages = ref(10)
+  const activePage = ref(4)
+
+  const prevPage = () => {
+    if (activePage.value > 1) {
+      activePage.value--
+    }
+  }
+
+  const nextPage = () => {
+    if (activePage.value < pages.value) {
+      activePage.value++
+    }
+  }
+</script>
+
+<template>
+  <h1>Pagination</h1>
+</template>
