15d80fb82dd4f4c615ee032f6dae9d15b4dee848
hchavezj <hchavezj@gmail.com>
2022-09-14 22:44:05 -0500

    Updated MainPagination.vue

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/script-indent -->
 <script setup>
   import { ref } from 'vue'
   const pages = ref(10)
@@ -17,5 +18,26 @@
 </script>
 
 <template>
-  <h1>Pagination</h1>
+  <div class="pagination">
+    <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
+    <button class="page" v-for="page in pages" :class="page === activePage ? 'active' : ''" @click="activePage = page">
+      {{ page }}
+    </button>
+    <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
+  </div>
 </template>
+
+<style lang="postcss" scoped>
+  .pagination {
+    @apply flex justify-center gap-4;
+    .action {
+      @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200 disabled:text-slate-400 hover:disabled:bg-slate-100;
+    }
+    .page {
+      @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200;
+      &.active {
+        @apply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
+      }
+    }
+  }
+</style>
